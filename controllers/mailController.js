287504973d535e609f1c12fe0e10ba319c66960b
Fabre Thomas <fabre.tomas@hotmail.fr>
2018-06-01 12:41:33 +0200

    ajout EMAIL

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,3 +1,41 @@
+'use strict'
+
+const nodemailer = require('nodemailer')
+var transporter = null
+
 exports.send = function (req, res) {
-    return res.status(400).send('Hello world !! I use VSCODE now !');
+
+    if (!transporter) {
+        transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.EMAIL_PASS
+            }
+        });
+    }
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Charlie\'s Créa 👻" <contact@charliescrea.fr>', // sender address
+        to: 'fabre.tomas@hotmail.fr, fabre.marc@gmail.com', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world?', // plain text body
+        html: '<b>Hello world?</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send('erreur en envoyant le mail');
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        return res.status(400).send('mail envoyé');
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
 }
