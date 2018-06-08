aa34fb8f34bc85802d593415a61d6d7571fdcdca
Fabre Thomas <fabre.tomas@hotmail.fr>
2018-06-08 13:22:20 +0200

    a

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -325,6 +325,7 @@
 
             $('#option-select').on('click', '.image-button', function () {
                 $('#option').addClass('option-activ');
+                $window.scrollTop(0);
                 $('#option').show();
                 rang = $(this).attr('rang');
                 return rang;
