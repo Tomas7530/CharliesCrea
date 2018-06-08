06da8d78ae755181b8384013cc5bbdc73cedae25
Fabre Thomas <fabre.tomas@hotmail.fr>
2018-06-08 11:18:37 +0200

    a

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -22,7 +22,7 @@ exports.send = function (req, res) {
     }
 
     var html = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' +
-        '<html xmlns:v="urn:schemas-microsoft-com:vml">' +
+        '<html lang="fr" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">' +
 
         '<head>' +
         '<meta http-equiv="content-Type" content="text/html; charset=utf-8">' +
